a1148ef81ea68611a3c143179a749d016bb823e2
anson224 <nxc224@student.bham.ac.uk>
2024-12-11 15:17:36 +0000

    updated autorun files

--- java-temp/autorun_total_hour_suggestion.sql
+++ java-temp/autorun_total_hour_suggestion.sql
@@ -1,73 +1,73 @@
-WITH chore_records_durations AS (
-    SELECT
-        start_log.resident_id,
-        start_log.chore_type,
-        start_log.timestamp AS start_time, 
-        finish_log.timestamp AS finish_time,
-        finish_log.timestamp - start_log.timestamp AS duration
-    
-    FROM chores_log AS start_log
-    JOIN chores_log AS finish_log
-        ON 
-        start_log.resident_id = finish_log.resident_id
-        AND start_log.chore_type = finish_log.chore_type
-        AND start_log.action = 'start'
-        AND finish_log.action = 'finish'
-        AND start_log.timestamp < finish_log.timestamp
-
-    WHERE 
-        NOT EXISTS(
-            SELECT 1 
-            FROM chores_log AS any_log
-            WHERE any_log.resident_id = start_log.resident_id
-            AND any_log.chore_type = start_log.chore_type
-            AND any_log.action = 'start'
-            AND any_log.timestamp > start_log.timestamp
-            AND any_log.timestamp < finish_log.timestamp
-        )
+WITH resident_presence AS (
+    SELECT 
+        timestamp, 
+        action, 
+        SUM(CASE WHEN action = 'enter' THEN 1 ELSE -1 END) 
+        OVER (ORDER BY timestamp) AS net_presence
+    FROM access_log
 ),
+zero_to_one_transitions AS (
+    SELECT 
+        r1.timestamp AS start_timestamp, 
+        r2.timestamp AS end_timestamp
+    FROM resident_presence r1
+    JOIN resident_presence r2
+    ON r1.timestamp < r2.timestamp
+    WHERE r1.net_presence = 0 AND r2.net_presence = 1
+    AND NOT EXISTS (
+        SELECT 1 
+        FROM resident_presence r3
+        WHERE r3.timestamp > r1.timestamp AND r3.timestamp < r2.timestamp AND r3.net_presence != 0
+    )
+)
+INSERT INTO total_hour_suggestions (resident_id, start_timestamp, end_timestamp)
+SELECT 0 AS resident_id, start_timestamp, end_timestamp
+FROM zero_to_one_transitions
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM total_hour_suggestions 
+    WHERE total_hour_suggestions.start_timestamp = zero_to_one_transitions.start_timestamp
+      AND total_hour_suggestions.end_timestamp = zero_to_one_transitions.end_timestamp
+      AND total_hour_suggestions.resident_id = 0
+)
+ORDER BY start_timestamp;
 
-avg_duration_chore AS (
-    SELECT
-        resident_id,
-        chore_type,
-        AVG(duration) AS avg_duration
-    FROM chore_records_durations
-    GROUP BY resident_id, chore_type
-),
-
-resident_presence AS (
-    SELECT
+WITH resident_presence AS (
+    SELECT 
         resident_id, 
         timestamp, 
-        action
+        action, 
+        SUM(CASE WHEN action = 'enter' THEN 1 ELSE -1 END) 
+        OVER (ORDER BY timestamp) AS net_presence
     FROM access_log
 ),
 transitions AS (
     SELECT 
         resident_id, 
         timestamp AS start_timestamp, 
-        LEAD(timestamp) OVER (PARTITION BY resident_id ORDER BY timestamp) AS end_timestamp
+        LEAD(timestamp) OVER (ORDER BY timestamp) AS end_timestamp,
+        net_presence,
+        LEAD(net_presence) OVER (ORDER BY timestamp) AS next_net_presence
     FROM resident_presence
 ),
-filtered_periods_mec AS (
+filtered_periods AS (
     SELECT 
         resident_id, 
         start_timestamp, 
         end_timestamp
     FROM transitions
-    WHERE end_timestamp IS NOT NULL
-),
-
-chore_hours_suggestions AS (
-    SELECT
-        presence.resident_id,
-        chore.chore_type,
-        presence.start_timestamp,
-        presence.start_timestamp + INTERVAL '1 second' * EXTRACT(EPOCH FROM chore.avg_duration) AS suggested_end_timestamp
-    FROM filtered_periods_mec presence
-    JOIN avg_duration_chore chore
-        ON presence.resident_id = chore.resident_id
+    WHERE net_presence = 1 
+      AND (next_net_presence IS DISTINCT FROM 1 OR next_net_presence IS NULL)
 )
-
-SELECT * FROM chore_hours_suggestions;
+INSERT INTO total_hour_suggestions (resident_id, start_timestamp, end_timestamp)
+SELECT resident_id, start_timestamp, end_timestamp
+FROM filtered_periods
+WHERE end_timestamp IS NOT NULL
+  AND NOT EXISTS (
+      SELECT 1 
+      FROM total_hour_suggestions 
+      WHERE total_hour_suggestions.resident_id = filtered_periods.resident_id
+        AND total_hour_suggestions.start_timestamp = filtered_periods.start_timestamp
+        AND total_hour_suggestions.end_timestamp = filtered_periods.end_timestamp
+)
+ORDER BY start_timestamp;
